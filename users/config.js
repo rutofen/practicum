787b872ccae265900828df924795fd8f15aa2668
unknown <esti9282@gmail.com>
2024-07-03 03:06:13 +0300

    👷add_commit

---
+++ users/config.js
@@ -0,0 +1,15 @@
+require('dotenv').config();
+
+console.log('DB_NAME:', process.env.DB_NAME);
+console.log('DB_USERNAME:', process.env.DB_USERNAME);
+console.log('DB_PASSWORD:', process.env.DB_PASSWORD);
+console.log('DB_HOST:', process.env.DB_HOST);
+console.log('DB_PORT:', process.env.DB_PORT);
+
+module.exports = {
+    db_name: process.env.DB_NAME,
+    db_user: process.env.DB_USERNAME,
+    db_pass: process.env.DB_PASSWORD,
+    db_host: process.env.DB_HOST,
+    db_port: process.env.DB_PORT
+};
---
+++ users/server.js
@@ -0,0 +1,18 @@
+// const app = require('./app');
+// require('dotenv').config();
+
+// const port = process.env.PORT || 3005;
+// const host = process.env.HOST || '127.0.0.1';
+
+// app.listen(port, host, () => {
+//   console.log(`Server running at http://${host}:${port}/`);
+// });
+const app = require('./app');
+require('dotenv').config();
+
+const port = process.env.PORT || 3005;
+const host = process.env.HOST || '127.0.0.1';
+
+app.listen(port, host, () => {
+  console.log(`Server running at http://${host}:${port}/`);
+});
