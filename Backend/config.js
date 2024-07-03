f47fe06f733073b3e6aa093ee54d9bd55e93de48
avigail heber <avigail@gmail.com>
2024-07-03 16:36:59 +0300

    update and Spell checking

--- Backend/config.js
+++ Backend/config.js
@@ -6,12 +6,6 @@ const db_pass = process.env.DB_PASSWORD;
 const db_host = process.env.DB_HOST;
 const db_port = process.env.DB_PORT;
 
-console.log('DB_NAME:', db_name);
-console.log('DB_USERNAME:', db_user);
-console.log('DB_PASSWORD:', db_pass);
-console.log('DB_HOST:', db_host);
-console.log('DB_PORT:', db_port);
-
 module.exports = {
     db_name,
     db_user,
--- Backend/core/config_db.js
+++ Backend/core/config_db.js
@@ -15,20 +15,3 @@ const pool = new Pool({
 
 module.exports = pool;
  
-
-
-
-
-// const { db_name, db_user, db_pass, db_host, db_port } = require('../config');
-// const { Client } = require('pg');
-// require('dotenv').config();
-
-// const client = new Client({
-//   user: db_user,
-//   password: db_pass,
-//   host: db_host,
-//   port: db_port,
-//   database: db_name,
-// });
-
-// module.exports = client;
