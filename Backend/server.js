d4243c86cfaa87defe206e001d4d10ea375f4e8a
Rut Ofen <rutofen2269@gmail.com>
2024-07-04 10:25:12 +0300

    Update after conflicts2

--- Backend/server.js
+++ Backend/server.js
@@ -1,10 +1,5 @@
-<<<<<<< HEAD
 //server.js
 const app = require('./app')
-=======
-require('dotenv').config();
-const app = require('./app');
->>>>>>> 01b986917a439771d848ecea1f6ea3c66d832d17
 
 const port = process.env.PORT || 3010
 const host = process.env.HOST || '127.0.0.1'
