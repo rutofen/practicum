485de2c2c5ac8a241229d8d331ec323d576e7ec1
avigail heber <avigail@gmail.com>
2024-07-04 15:45:59 +0300

    add function

--- Backend/app.js
+++ Backend/app.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const transport_router = require('./routers/transport');
+
+const {create_transports_table} = require('./core/config_db');
 const app = express();
+create_transports_table();
 require('dotenv').config();
 
 app.use(bodyParser.json());
