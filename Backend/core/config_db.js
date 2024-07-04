c238977ddaa545214c6c9257bede40a5ccbaede3
avigail heber <avigail@gmail.com>
2024-07-05 00:06:30 +0300

    update create table function

--- Backend/core/config_db.js
+++ Backend/core/config_db.js
@@ -1,23 +1,3 @@
-// const {db_name,db_user,db_pass,db_host,db_port} = require('../config')
-
-// const pg = require('pg')
-// const { Pool, Client } = pg
-
-// const pool = new Pool({
-//   user: db_user,
-//   password: db_pass,
-//   host: db_host,
-//   port: db_port,
-//   database: db_name,
-// })
-
-
-
-// module.exports = pool;
-
-
-
-
 const { Pool } = require('pg')
 const { db_name, db_user, db_pass, db_host, db_port } = require('../config')
 
@@ -28,52 +8,34 @@ const pool = new Pool({
   port: db_port,
   database: db_name,
   ssl: false
-})
+});
 
-async function create_transports_table() {
-  const checkTableQuery = `
-    SELECT EXISTS (
-      SELECT 1
-      FROM information_schema.tables
-      WHERE table_name = 'transports'
-    );
+
+
+const create_transports_table = async () => {
+  const query = `
+      CREATE TABLE IF NOT EXISTS transport (
+         Id INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+         Date DATE NOT NULL,
+         Location_address TEXT NOT NULL,
+         Location_lng FLOAT,
+         Count_order INTEGER,
+         Additional_quantity INTEGER,
+         Status_id INTEGER REFERENCES Status_transport(Transport_status_id),
+         Driver_id INTEGER REFERENCES Drivers(Id),
+         Pump_id INTEGER REFERENCES Pumps(Id),
+         Pipe_count INTEGER,
+         Transfers TEXT
+      );
   `;
-
-  const createTableQuery = `
-    CREATE TABLE tracking (
-    Id INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    Date DATE NOT NULL,
-    Location_address TEXT NOT NULL,
-    Location_lng FLOAT,
-    Count_order INTEGER,
-    Additional_quantity INTEGER,
-    Status_id INTEGER REFERENCES Status_transport(Transport_status_id),
-    Driver_id INTEGER REFERENCES Drivers(Id),
-    Pump_id INTEGER REFERENCES Pumps(Id),
-    Pipe_count INTEGER,
-    Transfers TEXT
-    );
-  `
-  let client;
   try {
-    client = await pool.connect()
-    const result = await client.query(checkTableQuery)
-    const tableExists = result.rows[0].exists
-
-    if (!tableExists) {
-      await client.query(createTableQuery)
-      console.log('Created tracking table successfully')
-    } else {
-      console.log('Transports table already exists')
-    }
-  } catch (err) {
-    console.error('Error checking/creating transports table', err)
-  } finally {
-    if (client) {
-      client.release()
-    }
+    await pool.query(query);
+    console.log('Transport table created or already exists');
+  } catch (error) {
+    console.error('Error checking/creating transport table', error);
   }
-}
+};
+
 module.exports = {
   pool,
   create_transports_table,
