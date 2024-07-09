b5f4fb4da4a3f410273c738736c1d58842b1c2c8
EstiParser <150199317+EstiParser@users.noreply.github.com>
2024-07-09 12:53:22 +0300

    Update Backend/routers/usersRoutes.js
    Co-authored-by: Mentor10PTR <mentor10ptr@gmail.com>

--- Backend/routers/usersRoutes.js
+++ Backend/routers/usersRoutes.js
@@ -34,7 +34,7 @@ router.get('/getUser/:id', async (req, res) => {
 });
 
 
-router.put('/updateUser', async (req, res) => {
+router.put('/update', async (req, res) => {
     try {
         const updatedUser = await usersModules.updateUser(req.body);
         res.status(200).json(updatedUser);
