b675bbb323ac55ee30e3c805f9d5d67de38c32b4
Your Name <d0548491124@gmail.com>
2024-07-11 09:46:29 +0300

    Another function

--- Backend/core/Init_db.js
+++ Backend/core/Init_db.js
@@ -1,23 +1,26 @@
 const fs = require('fs').promises;
 const { createTableIfNotExists } = require('./config_db');
 
-async function createTablesFromJson(jsonPath) {
+async function readJsonFile(jsonPath) {
   try {
     const jsonData = await fs.readFile(jsonPath, 'utf-8');
-    const tables = JSON.parse(jsonData);
+    return JSON.parse(jsonData);
+  } catch (error) {
+    throw error;
+  }
+}
+async function createTablesFromJson(jsonPath) {
+    const tables = await readJsonFile(jsonPath);
 
     for (const table of tables) {
       const { tableName, columnsDefinition } = table;
       await createTableIfNotExists(tableName, columnsDefinition);
-    }
+    
 
     console.log('All tables created successfully');
-  } catch (error) {
-    console.error('Error creating tables from JSON:', error.message);
-    throw error;
-  }
-}
+
+}}
+
 module.exports = {
-    createTablesFromJson
-  };
-  
+  createTablesFromJson
+};
