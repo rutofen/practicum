bd525c028ca6f9258dee344d5bd932ac9aa85e5c
Rut <150802852+rutofen@users.noreply.github.com>
2024-07-15 11:46:29 +0300

    cleaning up file

--- Backend/core/Init_db.js
+++ Backend/core/Init_db.js
@@ -1,28 +1,28 @@
-// const jsonPath = '../Backend/data/db.json';
+const jsonPath = '../Backend/data/db.json';
 
-// const fs = require('fs').promises;
-// const { createTableIfNotExists } = require('./config_db');
+const fs = require('fs').promises;
+const { createTableIfNotExists } = require('./config_db');
 
-// async function readJsonFile() {
-//   try {
-//     const jsonData = await fs.readFile(jsonPath, 'utf-8');
-//     return JSON.parse(jsonData);
-//   } catch (error) {
-//     throw error;
-//   }
-// }
-// async function createTablesFromJson() {
-//     const tables = await readJsonFile();
+async function readJsonFile() {
+  try {
+    const jsonData = await fs.readFile(jsonPath, 'utf-8');
+    return JSON.parse(jsonData);
+  } catch (error) {
+    throw error;
+  }
+}
+async function createTablesFromJson() {
+    const tables = await readJsonFile();
 
-//     for (const table of tables) {
-//       const { tableName, columnsDefinition } = table;
-//       await createTableIfNotExists(tableName, columnsDefinition);
+    for (const table of tables) {
+      const { tableName, columnsDefinition } = table;
+      await createTableIfNotExists(tableName, columnsDefinition);
     
 
-//     console.log('All tables created successfully');
+    console.log('All tables created successfully');
 
-// }}
+}}
 
-// module.exports = {
-//   createTablesFromJson
-// };
+module.exports = {
+  createTablesFromJson
+};
