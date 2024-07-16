7268355e86de099ca96ff3ab50e2a9a62ad6bd55
Rut Ofen <rutofen2269@gmail.com>
2024-07-16 14:24:04 +0300

    � Added CI Workflow for ESLint using Docker

--- Backend/server.js
+++ Backend/server.js
@@ -7,8 +7,5 @@ const host = process.env.HOST || '127.0.0.1';
 app.listen(port, host, () => {
   console.log(`Server running at http://${host}:${port}/`);
 });
-<<<<<<< HEAD
 
 
-=======
->>>>>>> c085faf6b5db791b9747415932ad68e3efb5e7ee
