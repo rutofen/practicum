2396c73cdf814e3e5cda5707440260c6e12e5030
Rut Ofen <rutofen2269@gmail.com>
2024-07-17 10:49:46 +0300

    Added a lint.yml file to test ESLint in GitHub Action

--- Backend/.eslintrc.js
+++ Backend/.eslintrc.js
@@ -1,3 +1,10 @@
 module.exports = {
   extends: 'expo',
+  rules: {
+    "indent": ["error", 2],
+    "quotes": ["error", "single"],
+    "semi": ["error", "always"],
+    "no-unused-vars": ["warn"],
+    "no-console": ["warn"]
+  }
 };
